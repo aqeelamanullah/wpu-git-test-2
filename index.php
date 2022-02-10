055ba6ee1a58dc751a5e6063829dbb77732fc0b2
aqeelamanullah <aqeelamanullah15@gmail.com>
2022-02-10 13:26:38 +0700

    menambahkan paragraft

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est asperiores illum earum sequi placeat animi deleniti cum laboriosam corrupti maiores odio voluptate itaque tempore neque, quibusdam, exercitationem, quia tempora deserunt?</p>
+</body>
+</html>
