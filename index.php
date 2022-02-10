c183b1f7f931124a03d9639e770683f2c705d57c
aqeelamanullah <aqeelamanullah15@gmail.com>
2022-02-10 13:35:39 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est asperiores illum earum sequi placeat animi deleniti cum laboriosam corrupti maiores odio voluptate itaque tempore neque, quibusdam, exercitationem, quia tempora deserunt?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
